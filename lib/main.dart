2c7a57a94240e4e3d6a79991e507d0301216ad69
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-05-04 09:38:46 +0300

    devSt1

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+        backgroundColor: Colors.lightBlueAccent,
       ),
     );
   }
